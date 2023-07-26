3862b58fe540c55b0d8c40db6b7fb2c37a53c6ff
Krash Nagar <dpsvnkras12102@gmail.com>
2023-07-26 13:42:51 +0530

    bugs resolved in server.js

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const cookieParser=require("cookie-parser");
 const bodyParser=require("body-parser");
 const fileUpload=require("express-fileupload");
 const dotenv=require("dotenv");
+const cloudinary=require("cloudinary");
+const connectDatabase=require("./config/database");
 
 const errorMiddleware=require("./middleware/error");
 
 //Config
 dotenv.config();
-
 
 app.use(express.json());
 app.use(cookieParser());
@@ -35,5 +36,44 @@ app.get('/',(req,res)=>{
 
 // Middleware for Errors
 app.use(errorMiddleware);
+
+
+// Handling Uncaught Exceptions
+
+process.on("uncaughtException", (err)=>{
+    console.log(`Error : ${err.message}`);
+    console.log(`Shutting down the server due to Uncaught Exception`);
+    
+    server.close(()=>{
+        process.exit(1);
+    })
+})
+
+// Connecting to database
+connectDatabase();
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET,
+})
+
+const server=app.listen(process.env.PORT,()=>{
+    console.log(`server is working on http://localhost:${process.env.PORT}`)
+    // console.log(`server is working on http://localhost:${process.env.PORT}`)
+});
+
+
+
+
+// Unhandled Promise Rejection
+process.on("unhandledRejection",(err)=>{
+    console.log(`Error : ${err.message}`);
+    console.log(`Shutting down the server due to Unhandled Promise Rejections`);
+
+    server.close(()=>{
+        process.exit(1);
+    })
+});
 
 module.exports=app
